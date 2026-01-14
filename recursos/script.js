28f42743bcf1f8447fb121b6a7d62fbbb1fe6434
caiomonte <caiomm076@gmail.com>
2026-01-14 15:50:33 -0300

    estruturar atendimento

--- recursos/script.js
+++ recursos/script.js
@@ -1,10 +1,14 @@
+/* FUNÇÃO DE SLEEP */
+const esperar = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+
+/* LÓGICA DO SLIDESHOW */
 let indiceAtual = 0;
 let tempoIndice = [7000, 10000, 10000, 10000, 10000];
 const slides = document.querySelectorAll('.slide');
 const texto_primario = document.querySelectorAll('.texto-primario');
 const texto_secundario = document.querySelectorAll('.texto-secundario');
 
-const esperar = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 function mudarSlide(n) {
     slides[indiceAtual].classList.remove('ativo');
@@ -30,3 +34,56 @@ async function iniciarSlideshow() {
 }
 
 iniciarSlideshow();
+
+
+/* EXIBIR INFORMAÇÕES OCULTAS */
+const btnEtapas = document.getElementById('btn-etapas');
+const btnLocalizacao = document.getElementById('btn-localizacao');
+
+const etapasContainer = document.getElementById('etapas');
+const localizacaoContainer = document.getElementById('localizacao');
+
+let etapas = false;
+let localizacao = false;
+
+function mostrarEtapas() {
+    etapas = true;
+    
+    ocultarMapa();
+    etapasContainer.classList.add('ativo');
+}
+
+function mostrarMapa() {
+    localizacao = true;
+
+    ocultarEtapas();
+    localizacaoContainer.classList.add('ativo');
+}
+
+function ocultarEtapas() {
+    if (!etapas) return;
+
+    etapas = false;
+    etapasContainer.classList.remove('ativo');
+}
+
+function ocultarMapa() {
+    if (!localizacao) return;
+
+    localizacao = false;
+    localizacaoContainer.classList.remove('ativo');
+}
+
+btnEtapas.addEventListener('click', () => {
+    if (!etapas)
+        mostrarEtapas();
+    else
+        ocultarEtapas();
+});
+
+btnLocalizacao.addEventListener('click', () => {
+    if (!localizacao)
+        mostrarMapa();
+    else 
+        ocultarMapa();
+});
